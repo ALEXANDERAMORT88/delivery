bf21917f524f44863a75a04c453352abaada58e5
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2023-08-04 22:45:47 -0500

    middleware, .env, api.types, se estan implementando

--- BACKEND/index.ts
+++ BACKEND/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 import { routerApi } from './src/controllers/routes';
 
+dotenv.config();
 const app = express();
 const PORT = 3800;
 
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://alex88maa:delivery2023@cluster0.fyn7xr8.mongodb.net/')
+mongoose.connect(process.env.MONGO_URL as string)
 .then(() => {
     console.log("Conexión a Mongo establecida");
 })
--- BACKEND/src/controllers/customers.controller.ts
+++ BACKEND/src/controllers/customers.controller.ts
@@ -1,19 +1,25 @@
 import express from 'express';
+import { authenticateToken } from '../middlewares/jwt-validation';
 
 import {
     getCustomers,
 } from '../services/customers.service';
 
+import { CustomErrorFormat } from '../types/api.types';
+
 const router = express.Router();
 
-interface CustomErrorFormat {
-    code: number,
-    message: string,
-    errorMessage: unknown
-}
+router.get('', authenticateToken, async ( req, res ) => {
+    try {
+        const ServiceLayerResponse = await getCustomers();
 
-
-
+        res.status(ServiceLayerResponse.code).json({ result: ServiceLayerResponse.result});
+    } catch (error) {
+        const customError = error as CustomErrorFormat;
+        console.log(customError.errorMessage);
+        res.status(customError.code).json(customError.message);
+    }
+});
 
 export default router;
 
