7c9291a7335c17df9cd9f9e517f462b586e59827
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2023-08-10 21:59:34 -0500

    avance backend, y parte del navbar

--- BACKEND/src/data/collaborators.data.ts
+++ BACKEND/src/data/collaborators.data.ts
@@ -3,6 +3,7 @@ import { CollaboratorSchema } from "../schema/collaborators.schema";
 
 
 const readCollaborators = (): Promise <Collaborator[]> => {
+// eslint-disable-next-line no-async-promise-executor
 return new Promise(async (res, rej)=>{try {
     const mongoResponse = await CollaboratorSchema.find();
     res(mongoResponse);
